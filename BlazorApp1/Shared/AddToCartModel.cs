2ef95a5d6de27b889cc31f700c47502d016cb4ee
Aaron Cooper <aaron.cooper@red-gate.com>
2019-08-30 14:59:30 +0100

    Remove AddToCartModel

--- BlazorApp1/Shared/AddToCartModel.cs
+++ BlazorApp1/Shared/AddToCartModel.cs
@@ -4,14 +4,9 @@ using System.Text;
 
 namespace Blazor.Shared
 {
-    public class AddToCartModel
-    {
-        public string ProductSku { get; set; }
-        public int Quantity { get; set; }
-    }
     public class CartItemModel
     {
-        public ProductModel Product { get; set; }
+        public string ProductSku { get; set; }
         public int Quantity { get; set; } = 0;
     }
 }
